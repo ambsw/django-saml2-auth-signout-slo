e5991dc89f5c407a14ed8d42bc5575b3fa050b60
Clayton Daley <clayton.daley@gmail.com>
2019-09-27 09:54:09 -0500

    isolate NameID logic in plugin so main package can work without a NameID (i.e. for SSO and redirection)

--- django_saml2_auth_signout_slo/apps.py
+++ django_saml2_auth_signout_slo/apps.py
@@ -1,4 +1,7 @@
 from django import apps
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django_saml2_auth import signals, utils
 
 
 class AppConfig(apps.AppConfig):
@@ -8,3 +11,13 @@ class AppConfig(apps.AppConfig):
         # import plugins
         # noinspection PyUnresolvedReferences
         from . import plugins
+
+        if 'SLO' in settings.SAML2_AUTH.get('PLUGINS', {}).get('SIGNOUT', []):
+            # NameID is required
+            if settings.SAML2_AUTH.get('NAME_ID_FORMAT') in [None, 'None']:
+                raise ImproperlyConfigured(
+                    "django-saml2-auth-signout-slo requires a valid SAML2_AUTH.NAME_ID_FORMAT, not None"
+                )
+
+            # cache the NameID in the session
+            signals.before_authenticated.attach(utils.store_name_id)
